ec44f4f882adaef77618fcb8ee35eb1be272b732
ahsantarique <ahsantarique@ymail.com>
2019-01-16 17:42:21 +0600

    writing the fitness function

--- include/Fitness.hpp
+++ include/Fitness.hpp
@@ -1,9 +1,18 @@
 #ifndef FITNESS_HPP
 #define FITNESS_HPP
 
-class Fitness{
-    getScore(vector <int> path){
+#include "AllCities.hpp"
+#include <cmath>
 
+class Fitness{
+    double getDistance(const City &a, const City &b){
+        return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
+    }
+    double getScore(vector <int> path, const AllCities & allcities){
+        double score = 0;
+        for(int i = 0; i < path.size()-1; i++){
+            double step = getDistance(allcities.cities[i], allcities.cities[i+1]);
+        }
     }
 };
 
